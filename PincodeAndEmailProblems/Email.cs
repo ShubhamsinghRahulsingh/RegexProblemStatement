efc94907ba69f0aa34540f25487f66b7714bf4f2
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-30 13:03:49 +0530

    (Shubham)Refactor: validate Email first mandatory part

---
+++ PincodeAndEmailProblems/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PincodeAndEmailProblems
+{
+    public class Email
+    {
+        const string emailFirstPart = "^[a-z]";
+        public void ValidateEmailFirstPart(string input)
+        {
+            if (Regex.IsMatch(input, emailFirstPart))
+                Console.WriteLine("{0} as Email Id is valid", input);
+            else
+                Console.WriteLine("{0} as Email Id is invalid ", input);
+
+        }
+    }
+}
--- PincodeAndEmailProblems/Program.cs
+++ PincodeAndEmailProblems/Program.cs
@@ -6,11 +6,30 @@ namespace PincodeAndEmailProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Regex Pattern problems");
-            Pincode pin=new Pincode();
-            pin.ValidatePinCode("400088");
-            pin.restrictAtBeginning("B400088");
-            pin.restrictAtEnd("400088B");
-            pin.whiteSpaceAllowed("400 088");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Pincode Validation\n2.Email Validation\n3.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Pincode pin = new Pincode();
+                        pin.ValidatePinCode("400088");
+                        pin.restrictAtBeginning("B400088");
+                        pin.restrictAtEnd("400088B");
+                        pin.whiteSpaceAllowed("400 088");
+                        break;
+                    case 2:
+                        Email email = new Email();
+                        email.ValidateEmailFirstPart("abc");
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+               
+            }
         }
     }
 }
